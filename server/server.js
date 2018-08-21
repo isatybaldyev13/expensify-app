9b3908f20b0bafabb9e773f7f750f502d7357045
Iskender <iska.satybaldyev@gmail.com>
2018-08-21 18:49:57 +0300

    added new functions

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const publicPath = path.join(__dirname,'..','public')
+
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(publicPath,'index.html'))
+})
+
+app.use(express.static(publicPath))
+
+
+
+app.listen(3000,()=>{
+    console.log('server is running');
+})
